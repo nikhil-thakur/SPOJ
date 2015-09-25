94fb89cf0f5beee325d09d880234769d8de503ad
eric_carter <eric_carter@choicehotels.com>
2015-09-25 13:18:24 -0700

    SPOJ

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String line = bufferedReader.readLine();
+
+    }
+}
